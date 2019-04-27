7949259f4c9081ab674cfad293a1abd8cc6b9b67
jabungay <39632398+jabungay@users.noreply.github.com>
2019-04-26 22:56:00 -0230

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express  = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
+// node-aREST
+var rest = require("arest")(app);
+rest.addDevice('http','192.168.2.83');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage'
+  });
+});
+
+const server = app.listen(7000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
