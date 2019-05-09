c4510850d31d6dcbf54e2b6d650a5e6de5be7958
Jonathan Bungay <jabungay@mun.ca>
2019-05-09 19:31:33 -0230

    Added _H suffix to definition

--- Arduino/Nanoleaf/HelperFunctions.h
+++ Arduino/Nanoleaf/HelperFunctions.h
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
-#ifndef HELPERS
-#define HELPERS
+#ifndef HELPERS_H
+#define HELPERS_H
 
-
-
+// Seperates comma-delimited data given the data, an array to input to,
+// and the expected number of variables
 void seperateData(String data, String* arr, int arrLen);
 
 #endif
